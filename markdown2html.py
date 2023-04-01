fed5ccc70e7efd1b4f5621e6241e6e21b6e7cc2d
Caballero018 <alejoxcaballero18@gmail.com>
2023-04-01 13:38:08 -0500

    Script markdown2html.py that takes an argument 2 strings

--- markdown2html.py
+++ markdown2html.py
@@ -4,9 +4,9 @@ import os
 
 args = sys.argv
 if len(args) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file = sys.stderr)
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     sys.exit(1)
 if not os.path.exists(sys.argv[1]):
-    print("Missing {}".format(sys.argv[1]), file = sys.stderr)
+    print("Missing {}".format(sys.argv[1]), file=sys.stderr)
     sys.exit(1)
 sys.exit(0)
